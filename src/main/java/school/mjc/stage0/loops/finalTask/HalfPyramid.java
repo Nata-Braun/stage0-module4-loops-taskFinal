e7c758f394bb032ad2fddb4ac903df01c8b2601c
Nata Braun <pushistik2030@gmail.com>
2023-02-07 11:50:45 +0300

    The task is solved

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        String newLine;
+        for (int i = 1; i<=cathetusLength; i++){
+            newLine = "";
+            for (int j = 1; j<=cathetusLength; j++){
+                if (j<=cathetusLength-i) {
+                    newLine = newLine + " ";
+                }else {
+                    newLine = newLine +"*";
+                }
+            }
+            System.out.println(newLine);
+        }
 
     }
 
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,27 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        String newLine;
+        for (int i = 0; i<cathetusLength; i++){
+            newLine = "";
+            for (int j = 1; j <= cathetusLength*2-1; j++){
+
+                if (j<cathetusLength-i || j>cathetusLength+i) {
+                    if (j>cathetusLength+i){
+                        continue;
+                    }
+                    newLine = newLine + " ";
+
+                } else {
+                    if (j<=cathetusLength) {
+                        newLine = newLine + String.valueOf(cathetusLength - j + 1);
+                    } else {
+                        newLine = newLine + String.valueOf(j - cathetusLength+1);
+                    }
+                }
+            }
+            System.out.println(newLine);
+        }
 
     }
 
